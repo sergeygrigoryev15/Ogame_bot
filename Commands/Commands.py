f8a8c4b25f7263f9802bfeb6953b81b80f033521
Sergey Grigoryev <metalloman93@gmail.com>
2021-04-24 19:06:57 +0300

    add new screen

--- Commands/Commands.py
+++ Commands/Commands.py
@@ -1,4 +1,5 @@
 import random
+from datetime import timedelta
 
 from Core.BaseEntity import BaseEntity
 from Enums.FleetMissionTypes import FleetMissionTypes
@@ -7,6 +8,7 @@ from Screens.FleetDestinationScreen import FleetDestinationScreen
 from Screens.FleetMissionScreen import FleetMissionScreen
 from Screens.FleetMovementsScreen import FleetMovementsScreen
 from Screens.FleetScreen import FleetScreen
+from Screens.GalaxyScreen import GalaxyScreen
 from Screens.OverviewScreen import OverviewScreen
 
 
@@ -14,6 +16,12 @@ class Commands(BaseEntity):
     def save_fleet(self, planet):
         overview_screen = OverviewScreen()
         overview_screen.planets_list.select_planet(planet)
+        planets = overview_screen.planets_list.data.keys()
+        if len(planets) > 1:
+            destination_planet = random.choice([p for p in planets if p is not planet])
+        else:
+            solar_system_planets = self.get_planets_in_my_system()
+            destination_planet = solar_system_planets[0].position # if solar_system_planets else make expedition
         overview_screen.navigation_menu.open_tab(MenuTabs.FLEET)
         fleet_screen = FleetScreen()
         if fleet_screen.has_fleet:
@@ -22,8 +30,6 @@ class Commands(BaseEntity):
 
             fleet_destination_screen = FleetDestinationScreen()
             fleet_destination_screen.select_speed(10)
-            planets = fleet_destination_screen.planets_list.data.keys()
-            destination_planet = random.choice([p for p in planets if p is not planet])
             fleet_destination_screen.select_destination(destination_planet)
             fleet_destination_screen.go_next()
 
@@ -45,6 +51,9 @@ class Commands(BaseEntity):
             fleet_screen.navigation_menu.open_tab(MenuTabs.OVERVIEW)
 
     def return_fleet(self):
+        # TODO when saving fleet, remember time of sending ships.
+        #  when returning fleet, return time will be current_time - time of sending.
+        #  if return_time < N* enemy remaining_time, then return it
         planets = self.db.get_return_fleet_queue()
         overview_screen = OverviewScreen()
         log = overview_screen.fleetAlertsTab.get_log()
@@ -54,13 +63,20 @@ class Commands(BaseEntity):
             log,
         )
         for planet in [
-            p for p in planets if p not in [el.to_coordinates for el in enemy_fleets]
+            p for p in set(planets) if p not in [el.to_coordinates for el in enemy_fleets if el.remaining_time < timedelta(hours=1)]
         ]:
             overview_screen.navigation_menu.open_tab(MenuTabs.FLEET_MOVEMENTS)
             fleet_movements_screen = FleetMovementsScreen()
             fleet_movements_screen.return_fleet(from_coordinates=planet)
             self.db.delete_returned_fleet(planet)
             self.slack_bot.send_message(f'Fleet returning to planet {planet}.')
+
+    def get_planets_in_my_system(self):
+        overview_screen = OverviewScreen()
+        overview_screen.navigation_menu.open_tab(MenuTabs.GALAXY)
+        galaxy_screen = GalaxyScreen()
+        planets = galaxy_screen.get_planets()
+        return list(filter(lambda planet: not any([planet.owner.on_vocations, planet.owner.admin]), planets))
 
 
 commands = Commands()
---
+++ Screens/GalaxyScreen.py
@@ -0,0 +1,38 @@
+from typing import Union
+
+from dataclasses import dataclass
+from selenium.webdriver.common.by import By
+
+from Elements.WebElement import WebElement
+from Screens.BaseOgameScreen import BaseOgameScreen
+
+
+@dataclass
+class PlanetOwner:
+    name: str
+    on_vocations: bool
+    noob: bool
+    admin: bool
+
+
+@dataclass
+class Planet:
+    position: Union[list, tuple]
+    name: str
+    has_moon: bool
+    has_debris: bool
+    owner: PlanetOwner
+
+
+class GalaxyScreen(BaseOgameScreen):
+    def __init__(self):
+        super().__init__('//*[@id="galaxycomponent"]')
+
+        self.txb_galaxy = WebElement('galaxy_input', id_type=By.ID)
+        self.txb_solar_system = WebElement('system_input', id_type=By.ID)
+        self.btn_go = WebElement('btn_blue', id_type=By.CLASS_NAME)
+
+    def get_planets(self):
+        # TODO make this work properly
+        return [Planet([1, 29, 4], 'Planet1', False, False, PlanetOwner('Homeless', False, False, False))]
+
