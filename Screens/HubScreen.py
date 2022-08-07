4f6a262e6362993b87411ef36231d8674163794b
Sergey Grigoryev <metalloman93@gmail.com>
2022-08-07 18:07:38 +0300

    If environment variable UNIVERSE exists, then the game in it would be opened, if variable does not exists or empty, would be opened last played universe.

--- Screens/HubScreen.py
+++ Screens/HubScreen.py
@@ -1,6 +1,10 @@
 import time
+from typing import Optional
+
+from selenium.webdriver.common.by import By
 
 from Core.BaseScreen import BaseScreen
+from Core.Env import environ
 from Elements.WebElement import WebElement
 
 XPATH = '//*[@id="hub"]'
@@ -11,14 +15,25 @@ class HubScreen(BaseScreen):
         BaseScreen.__init__(self, XPATH)
 
         self.btn_continue = WebElement('//button[contains(@class, "button-default")]')
+        self.btn_play = WebElement('//button[contains(@class, "button-primary")]')
+        self.my_accounts = WebElement('myAccounts', By.ID)
+
+        self.tmp_continue_in_universe = './/*[@role="rowgroup"]' \
+                                        '[.//*[contains(@class, "server-name")]/*[contains(.,"{}")]]//button'
 
     @staticmethod
     def is_open():
         return WebElement(XPATH).is_present()
 
-    def continue_game(self):
+    def continue_game(self, universe: Optional[str] = environ('UNIVERSE', default=None)):
         time.sleep(3)
-        self.btn_continue.click()
-        time.sleep(5)
+        if universe:
+            self.btn_play.click()
+            time.sleep(1)
+            btn = self.my_accounts.find_element_by_xpath(self.tmp_continue_in_universe.format(universe))
+            btn.click()
+        else:
+            self.btn_continue.click()
+        time.sleep(3)
         self.driver.close()
-        self.driver.switch_to_window(self.driver.window_handles[-1])
+        self.driver.switch_to.window(self.driver.window_handles[-1])
